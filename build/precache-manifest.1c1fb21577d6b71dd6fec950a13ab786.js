a2b459c31dfc37d565a7a3979baf5eb2669d3e81
guru2711 <59472229+guru2711@users.noreply.github.com>
2021-09-17 19:57:27 +0530

    Add files via upload

---
+++ build/precache-manifest.1c1fb21577d6b71dd6fec950a13ab786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aedf2b5d5ba361fad9ddd3d5145056d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ada96abd7173227bdcf",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6ada96abd7173227bdcf",
+    "url": "/static/js/2.74abc827.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.74abc827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb7ab0ae6d91af75fe3e",
+    "url": "/static/js/main.94e0af93.chunk.js"
+  },
+  {
+    "revision": "1d05d21cfb7c11100d5b",
+    "url": "/static/js/runtime-main.5be0474a.js"
+  }
+]);
